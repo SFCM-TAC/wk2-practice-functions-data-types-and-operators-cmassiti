0fa0397881c7df2cf5fdf2cca821b841b9039df9
cmasaitis <cmasaitis@TACs-MacBook-Pro.local>
2018-09-19 14:13:14 -0700

    1) Incorporated var result to document.write and return "Hello Cole!!" (hello + name + !!)
    2) Incorporated var threeNames and var result to document.write and 
    return "Hello Cole, MaryClare, and Adrema Justice!!" (hello + name + !!)
    
    3)Created var classes and var result = getClasses(classes)
    Return classes (classes = (list of course schedule written out in 
    quotes) + "." /
    Requested document.write(result)
    
    4)Created var fiveNumbers, then var result = labelOrder(fivenumbers)/
    - Made fiveNumbers = [1-5] array
    - Made var called newArray with empty quotation marks for each of the 
    five Numbers
    - Created newArray indexes [0-4] and made them equal to "first-fifth" + 
    fiveNumbers[0-5] all individually
    -return(newArray)
    
    5)set variables w, l, and h = to 3, 6, 9
    -created a variable for volume = w * l * h
    - returned (volume)
    -Requested document.write(result) for var result = calculateVolume()
    
    6) Requested variable n;, var result = isOdd(), and then 
    document.write(result)
    - set var n + 4
    -then stated if (n%1 == 0)
    return true;
    else
    return false;
    
    7)Set var result = getSalePrices(used originalPrice (4.00), then said to 
    document.write result based on... - 
    -using a const saleAmount of .25
    - using the function getSalePrice from (originalPrice)
    -Set the saleAmount equal to OriginalPrice subtracting (the 
    originalPrice * .25)
    -returning saleAmount
    
    8)Set var result = getSalePrices([used three differentprices]), then 
    said to document.write result based on... - 
    - a const salesAmounts = .25
    - having the function getSalePrices (from the threeOriginalPrices)
    - then making three variables for price one-three and setting them equal 
    to the three index numbers of the array
    - then three more variables for salesAmountsOne-Three and making them 
    equal to getSalePrice(from priceOne-Three)
    - Requested a return of [salesAmountsOne-Three]
    
    9) In the HTML only adding incrementCounter(); between the script 
    opening and closing then... - 
    - In .js having a var counter = 0
    - leaving function incrementCounter(empty)
    - added counter++ to add 1 increment
    - then added showCounter(added counter)
    -then named (increment) inside function showcounter at the bottom before 
    document.getElementByID etc.
    
    Thanks!

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,14 @@
 
 function sayHello(name) {
+  return ('Hello, ' + name + "!!")
   // TODO: return a greeting string that greets
   // the person by name, e.g. "Hello, Emily!"
 }
 
 function sayHelloEverybody(threeNames) {
+  threeNames = ['Cole', ' MaryClare', ' and Adrema Justice'];
+  return ('Hello ' + threeNames + '!!')
+
   // TODO: return a greeting string that greets
   // all three people in the threeNames array
   // e.g. "Hello, Emily, MaryClare, and Taurin!"
@@ -12,11 +16,27 @@ function sayHelloEverybody(threeNames) {
 }
 
 function getClasses() {
+  classes = [ 'Sound Design', ' Private Composition', ' Intro to Logic',
+ ' Tools, Techniques, and Analysis', ' Intro to Recording', ' Composition Workshop',
+ ' Computer Science', ' and Music Business'];
+  return (classes + ".")
+
   // TODO: return an array with a list of strings
   // naming the classes you are taking this semester
 }
 
 function labelOrder(fiveNumbers) {
+  var fiveNumbers = [1,2,3,4,5];
+  var newArray = ["","","","",""];
+
+  newArray[0] = "first: " + fiveNumbers[0];
+  newArray[1] = " second: " + fiveNumbers[1];
+  newArray[2] = " third: " + fiveNumbers[2];
+  newArray[3] = " fourth: " + fiveNumbers[3];
+  newArray[4] = " fifth: " + fiveNumbers[4];
+
+  return (newArray)
+
   // TODO: Given an array with five numbers, return a new array
   // of strings that have labels for the ordering.
   // e.g. if fiveNumbers is [1,2,3,4,5] your function should return:
@@ -25,27 +45,54 @@ function labelOrder(fiveNumbers) {
 }
 
 function calculateVolume(w, l, h) {
+
+  var w = 3
+  var l = 6
+  var h = 9
+  var volume = w * l * h
+
+  return (volume)
+
   // TODO: given a room's width, length, and height
   // write a function that returns the volume
 }
 
 function isOdd(n) {
-  // TODO: given a number, return true if it is odd, false if it is even
-  // HINT: the expression "Boolean(0)" is false, "Boolean(1)"" is true
-  // DOUBLE HINT: Try using the modulus operator (%)
+  var n = 4
+
+	if (n%1 == 0)
+		return true;
+	else
+		return false;
 }
 
 // ** CART PRICES ** //
 
 const saleAmount = .25 // 25% off!
 function getSalePrice(originalPrice) {
+  var saleAmount;
+
+  saleAmount = originalPrice - (originalPrice * .25)
+
+  return saleAmount;
+
   // TODO: given a dollar price (e.g. 5.99), return
   // a price with the saleAmount discount applied
   // e.g. if originalPrice is 4.00, your function should return 3.00.
   // (Don't worry about rounding to the correct number of decimal places)
 }
 
+const salesAmounts = .25
 function getSalePrices(threeOriginalPrices) {
+  var priceOne = threeOriginalPrices[0];
+  var priceTwo = threeOriginalPrices[1];
+  var priceThree = threeOriginalPrices[2];
+  var salesAmountsOne = getSalePrice(priceOne);
+  var salesAmountsTwo = getSalePrice(priceTwo);
+  var salesAmountsThree = getSalePrice(priceThree);
+
+  return [salesAmountsOne, salesAmountsTwo, salesAmountsThree];
+
   // TODO: given an array of prices for three items in a cart,
   // return a new array with the saleAmount discount applied to each.
   // TIP: see if you can reuse the getSalePrice function that you just wrote!
@@ -56,12 +103,17 @@ function getSalePrices(threeOriginalPrices) {
 
 var counter = 0;
 function incrementCounter() {
+
+counter++;
+showCounter(counter);
+
   // TODO: implement a function that increments (adds 1) to the counter variable
   // and then calls showCounter to update the interface with the new value.
   // Test your code by opening the developer console and calling incrementCounter.
   // The displayed answer for this question should increase by 1!
 }
-function showCounter() {
+
+function showCounter(increment) {
   // updates the counter element with the latest value
   document.getElementById('counter').textContent = counter;
 }
